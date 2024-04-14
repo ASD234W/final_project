fc4e3205025a6f64e034789dd044b3b188f8ecce
ASD234W <100771005+ASD234W@users.noreply.github.com>
2024-04-14 20:09:50 +0800

    comment

--- server.py
+++ server.py
@@ -1,36 +1,20 @@
 #simple websockets brocaster
 import asyncio
 import websockets
-clients = [] #to store all connected cleints
 
-#handler for socket message activities
+# A list to store all connected clients
+clients = []  # type: List[websockets.WebSocketServerProtocol]
+
+# Handler function for socket message activities
 async def handler(websocket, path):
-	#print(path) #path is not used currently
-	if websocket not in clients:
-		clients.append(websocket) #append new cleint to the array
-		
-	async for message in websocket:
-		print(message,'received from client') #print to console
-		await brocast(message) #send message to all clents
+    #print(path)  # path is not used currently
+    
+    # Add the new client to the list of clients only if it's not already present
+    if websocket not in clients:
+        clients.append(websocket)  # append new client to the array
 
-async def brocast(msg):
-	print(msg,' brocasting') #print to console
+    # Listen for messages from the client
+    async for message in websocket:
+        print(message, 'received from client')  # print to console
+        await brocast(message)  # send message to all clients
 
-	#iterate the clients
-	for websock in clients:
-		try:
-			await websock.send(msg) #send message to each client
-		except websockets.exceptions.ConnectionClosed:
-			#remove the client when it disconnects
-			print("Client disconnected.  Do cleanup")
-			clients.remove(websock)
-			#pass
-
-#starts the service and run forever
-loop = asyncio.new_event_loop() #get an event loop
-asyncio.set_event_loop(loop) #set the event loop to asyncio
-
-loop.run_until_complete(
-	websockets.serve(handler, 'localhost', 4545) #setup the websocket service and handler
-	) #hook to localhost:4545
-loop.run_forever() #keep it running
