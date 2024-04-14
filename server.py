e2470878f8fa8142bc52d9dc33bc46a2b4310eef
ASD234W <100771005+ASD234W@users.noreply.github.com>
2024-04-14 20:10:46 +0800

    improve

--- server.py
+++ server.py
@@ -1,20 +1,27 @@
-#simple websockets brocaster
+# Simple WebSockets Brocaster
 import asyncio
 import websockets
 
-# A list to store all connected clients
-clients = []  # type: List[websockets.WebSocketServerProtocol]
+# A set to store all connected clients
+clients = set()  # type: Set[websockets.WebSocketServerProtocol]
 
 # Handler function for socket message activities
 async def handler(websocket, path):
-    #print(path)  # path is not used currently
-    
-    # Add the new client to the list of clients only if it's not already present
+    # Add the new client to the set of clients only if it's not already present
     if websocket not in clients:
-        clients.append(websocket)  # append new client to the array
+        clients.add(websocket)  # add new client to the set
 
     # Listen for messages from the client
     async for message in websocket:
         print(message, 'received from client')  # print to console
         await brocast(message)  # send message to all clients
 
+    # Remove the client from the set when it disconnects
+    clients.remove(websocket)
+
+
+# Function to brocast message to all clients
+async def brocast(message):
+    for client in clients:
+        if client.open:
+            await client.send(message)
